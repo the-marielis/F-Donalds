a536d5242bc9824ace268a48823b788c98d236e0
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2025-02-24 13:21:51 -0300

    commits iniciais de carrinho de compras

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -9,23 +9,21 @@ import {
 } from "@/components/ui/sheet";
 
 import { CartContext } from "../contexts/cart";
+import CartProductItem from "./cart-product-item";
 
 const CartSheet = () => {
   const { isOpen, toggleCart, products } = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-      <SheetContent>
+      <SheetContent className="w-[85%]">
         <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetTitle>Sacola</SheetTitle>
           <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+
           </SheetDescription>
         </SheetHeader>
         {products.map((product) => (
-          <h1 key={product.id}>
-            {product.name} - {product.quantity}
-          </h1>
+          <CartProductItem key={product.id} product={ product } />
         ))}
       </SheetContent>
     </Sheet>
